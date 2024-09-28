d1fafaabb58e60870718d9f95c790664ec77c788
CSuperlei <leicai@pku.edu.cn>
2024-09-28 20:46:43 +0800

    Update ft_1d.py
    ft_1d_updated

--- ft_1d.py
+++ ft_1d.py
@@ -1,21 +1,19 @@
 '''
 Author: CSuperlei
-Date: 2023-08-12 15:28:55
+Date: 2024-09-28 15:28:55
 LastEditTime: 2024-02-15 17:23:56
 Description: Theoretical proof of the feasibility of HIFT principles
 '''
 import numpy as np 
 from scipy.signal import get_window 
 import matplotlib.pyplot as plt 
-
-plt.rcParams['pdf.fonttype'] = 'truetype'
-plt.rcParams["font.family"] = "Arial"
-plt.rcParams["font.size"] = 12
-plt.rcParams["axes.titlesize"] = 12
-plt.rcParams["axes.labelsize"] = 12
-plt.rcParams["xtick.labelsize"] = 12
-plt.rcParams["ytick.labelsize"] = 12
-plt.rcParams["legend.fontsize"] = 12
+from matplotlib.pyplot import plot, savefig, figure
+#plt.rcParams["font.size"] = 12
+#plt.rcParams["axes.titlesize"] = 12
+#plt.rcParams["axes.labelsize"] = 12
+#plt.rcParams["xtick.labelsize"] = 12
+#plt.rcParams["ytick.labelsize"] = 12
+#plt.rcParams["legend.fontsize"] = 12
 
 def gen_tri(signal_length, vo_fre_interval=50, flag='cos'):
     '''
@@ -127,20 +125,29 @@ if __name__ == '__main__':
     frameTime_X = frame_time(num_frames, window_size, hop_size, fs)
     freScale_Y = fre_scale(window_size, fs)
 
-     ## FFT theoretical result of HIFT
-    plt.subplot(2,1,1)
-    plt.plot(freq_X, Y)
-    plt.xlabel('Frequency')
-    plt.ylabel('Amplitude')
-    plt.title('FFT Magnitude Spectrogram')
+    ## FFT theoretical result of HIFT
+    fig = plt.figure(figsize=(10, 8)) 
+    ax0 = fig.add_subplot(311)
+    ax0.set_xlabel('Time')
+    ax0.set_ylabel('Value')
+    ax0.plot(t, signal)
+    ax0.set_title('Origin Signal')
+
+    ax1 = fig.add_subplot(312)
+    ax1.plot(freq_X, Y)
+    ax1.set_xlabel('Frequency')
+    ax1.set_ylabel('Amplitude')
+    ax1.set_title('HIFT 1D-FT Spectrum')
 
     # STFT theoretical result of HIFT
-    plt.subplot(2,1,2)
-    plt.pcolormesh(frameTime_X, freScale_Y, specgram, shading='gouraud', cmap='viridis')
-    plt.colorbar(label='Magnitude')
-    plt.xlabel('Time')
-    plt.ylabel('Frequency')
-    plt.title('STFT Magnitude Spectrogram')
+    ax2 = fig.add_subplot(313)  
+    ax2.pcolormesh(frameTime_X, freScale_Y, specgram, shading='gouraud', cmap='viridis')
+    ax2.set_xlabel('Time')
+    ax2.set_ylabel('Frequency')
+    ax2.set_title('HIFT 1D-STFT Sepctrum')
 
     plt.tight_layout()
+    #print('save results')
+    plt.savefig('./HIFT_FT_STFT.png')
+    #savefig('../results/HIFT_FT_STFT.png')
     plt.show()
